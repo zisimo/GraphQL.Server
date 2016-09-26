16ea295faf3cfdd4fb0bc447008bf898c06e32e8
Andries Coetzee <andriesc@lime-square.net>
2016-09-26 21:33:19 +0200

    Added initial projects

---
+++ GraphQL.Server.Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphQL.Server.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
