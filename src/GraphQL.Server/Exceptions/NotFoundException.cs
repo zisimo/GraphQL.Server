3c7ae6dc63482227b5ece35d1e4a6ba290f05a55
Andries Coetzee <andriesc@lime-square.net>
2017-03-28 19:34:07 +0200

    Changed not found exception to support different id types

--- src/GraphQL.Server/Exceptions/NotFoundException.cs
+++ src/GraphQL.Server/Exceptions/NotFoundException.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Linq;
-
-namespace GraphQL.Server.Exceptions
+﻿namespace GraphQL.Server.Exceptions
 {
     public class NotFoundException<T> : GraphException where T : class
     {
         public T[] Entities { get; private set; }
-        public int?[] Ids { get; private set; }
+        public object[] Ids { get; private set; }
 
-        public NotFoundException(int?[] ids) : base($"{typeof(T).Name}s not found[{string.Join(", ", ids)}]")
+        public NotFoundException(object[] ids) : base($"{typeof(T).Name}s not found[{string.Join(", ", ids)}]")
         {
             Ids = ids;
         }
-        public NotFoundException(int[] ids) : base($"{typeof(T).Name}s not found[{string.Join(", ", ids)}]")
-        {
-            Ids = ids.OfType<int?>().ToArray();
-        }
-        public NotFoundException(int? id) : base($"{typeof(T).Name} not found[{id}]")
+        public NotFoundException(object id) : base($"{typeof(T).Name} not found[{id}]")
         {
             Ids = new[] { id };
         }
