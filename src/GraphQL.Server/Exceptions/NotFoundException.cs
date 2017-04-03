5029a40952df4614bb016fec1636e31edb07d16b
Andries Coetzee <andriesc@lime-square.net>
2017-04-03 17:37:11 +0200

    Improved error message handling

--- src/GraphQL.Server/Exceptions/NotFoundException.cs
+++ src/GraphQL.Server/Exceptions/NotFoundException.cs
@@ -1,17 +1,24 @@
-﻿namespace GraphQL.Server.Exceptions
+﻿using System.Collections;
+using System.Linq;
+
+namespace GraphQL.Server.Exceptions
 {
     public class NotFoundException<T> : GraphException where T : class
     {
-        public T[] Entities { get; private set; }
         public object[] Ids { get; private set; }
-
-        public NotFoundException(object[] ids) : base($"{typeof(T).Name}s not found[{string.Join(", ", ids)}]")
-        {
-            Ids = ids;
-        }
-        public NotFoundException(object id) : base($"{typeof(T).Name} not found[{id}]")
+        
+        public NotFoundException(object id) : base($"{typeof(T).Name} not found[{GetIdString(id)}]")
         {
             Ids = new[] { id };
+        }
+
+        private static string GetIdString(object id)
+        {
+            if (id is IEnumerable)
+            {
+                return string.Join(", ", (id as IEnumerable).OfType<object>().Select(i => $"{i}"));
+            }
+            return $"{id}";
         }
     }
 }
